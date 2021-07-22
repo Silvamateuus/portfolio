a50c790f22076a38bddaa224da3fbf7c0cc90824
Mateus Silva <77-mateus@users.noreply.gitlab.dcide.info>
2021-07-22 19:23:19 -0300

    ATualizando

--- js/demo.js
+++ js/demo.js
@@ -14,15 +14,3 @@ document.addEventListener('DOMContentLoaded', function () {
 }, false);
 
 
-/*
-// jQuery plugin example:
-$(document).ready(function() {
-  $('#particles').particleground({
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
-  $('.intro').css({
-    'margin-top': -($('.intro').height() / 2)
-  });
-});
-*/
